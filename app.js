b8b41c93e38014358a370c361de765fe8f14d1ec
franck86 <fpagano1986@gmail.com>
2019-08-19 01:24:36 +0200

    fix

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ const fs 			= require('fs');
 const request 		= require('request');
 const cheerio 		= require('cheerio');
 const shortUrl 		= require('node-url-shortener');
+const normalize		= require('normalize-text');
+const MOMENT      	= require('moment-timezone');
+const MongoClient 	= require('mongodb').MongoClient;
 
 const TELEGRAF		= require('telegraf');
-let TOKEN_BOT		= '902859650:AAHL6K4MlPAh9iAWVOH2DZlRvjKgZ5XZDu4';
+const TOKEN_BOT		= '902859650:AAHL6K4MlPAh9iAWVOH2DZlRvjKgZ5XZDu4';
+const dbURI    		= 'mongodb://localhost:27017/ecommerce';
 
 const CHANNEL_ID  = -1001240502570;
 const MASTER_ID   = 973946580;
@@ -18,14 +22,34 @@ APP.use(BODY_PARSER.json());
 BOT.use(TELEGRAF.log());
 
 try {
+	const connectToDatabase = () => {
+		return new Promise((resolve, reject) => {
+			MongoClient.connect(dbURI, (err, db) => {
+				if (err) {
+					reject(err);
+				}
+			
+				resolve(db);
+			});
+		});
+	};
+
 	var getMyAffiliationByURL = async (requestUrl) => {
 		var amzn_url = 'http://www.amazon.it/dp/';
 		var promise = new Promise((resolve, reject) => {
 			request(requestUrl, function(error, response, body) {
 				if (!error && response.statusCode == 200) {
 					var $ = cheerio.load(body);
-					var previewImage = $('#landingImage')[0].attribs['data-old-hires'];
-					var asin = $('#cerberus-data-metrics')[0]['attribs']['data-asin'];
+					var title 			= $('#titleSection #productTitle').text().trim();
+					var previewImage 	= $('#landingImage')[0].attribs['data-old-hires'];
+					var asin 			= $('#cerberus-data-metrics')[0]['attribs']['data-asin'];
+					var stars			= $('#averageCustomerReviews .a-icon-star').text();
+					stars = parseInt(stars.split(' ')[0]);
+					
+					var category 		= $('#wayfinding-breadcrumbs_feature_div ul').text();
+					var catSplit = category.replace(/\r?\n|\r/g, '').split('›');
+					category = normalize.normalizeWhitespaces(catSplit[0]);
+					
 					var affUrl = amzn_url + asin + '?tag=shockprice05-21';
 	
 					shortUrl.short(affUrl, function(err, shortUrl){
@@ -33,9 +57,16 @@ try {
 							reject();
 						}
 						else{
+							var currentDate = MOMENT().tz('Europe/Rome').format('DD-MM-YYYY');
 							resolve({
-								affUrl : shortUrl ? shortUrl : affUrl,
-								image  : previewImage
+								_id			: asin + '_' + currentDate,
+								title		: title,
+								stars		: stars,
+								asin		: asin,
+								url 		: shortUrl ? shortUrl : affUrl,
+								img_url		: previewImage,	
+								category 	: category,
+								date	    : currentDate
 							});
 						}
 					});
@@ -66,6 +97,38 @@ try {
 		})
 	};
 
+	var makeid = (length) => {
+		var result           = '';
+		var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+		var charactersLength = characters.length;
+		for ( var i = 0; i < length; i++ ) {
+		  result += characters.charAt(Math.floor(Math.random() * charactersLength));
+		}
+		return result;
+	};
+  
+	var generateAccountId = () => {
+	  return makeid(8).toLowerCase();
+	};
+
+	var pushNewProductInDB = (product) => {
+		var promise = new Promise((resolve, reject) => {
+			connectToDatabase().then((db) => {
+				db.collection('item').insertOne(product).then(() => {
+					resolve();
+				})
+				.catch(() => {
+					reject();
+				});
+			})
+			.catch((err) => {
+				reject();
+			});
+		});
+
+		return promise;
+	};
+
 	var normalizeString = (string) => {
 		return string.replace(/<\/?[^>]+(>|$)/g, "");
 	};
@@ -81,14 +144,26 @@ try {
 					var amzUrl = urlify(data);
 					if(amzUrl.indexOf('https://amzn.to/') !== -1){
 						var affiliation = await getMyAffiliationByURL(amzUrl);
-						var newTMPL = normalizeString(data.replace(amzUrl, affiliation.affUrl));
-						newTMPL = `[​​​​​​​​​​​](${affiliation.image})${newTMPL}`;
-						TELEGRAM.sendMessage(CHANNEL_ID, newTMPL, { parse_mode : 'markdown' }).then(() => {
-							TELEGRAM.sendMessage(MASTER_ID, `Post ${path} pubblicato!!`, { parse_mode : 'html' });
-							removeFile(path);
-						}).catch((error) => {
-							TELEGRAM.sendMessage(MASTER_ID, `ERRORE Post ${path} NON pubblicato!!\n${JSON.stringify(error)}`, { parse_mode : 'html' });
-						});
+						if(affiliation && affiliation.url){
+							var newTMPL = normalizeString(data.replace(amzUrl, affiliation.url));
+							affiliation.tmpl = newTMPL;
+							affiliation.tmpl = affiliation.tmpl.replace(/\n/g, '<br/>').replace(/\t/g, '&ensp');
+							pushNewProductInDB(affiliation).then(() => {
+								newTMPL = `[​​​​​​​​​​​](${affiliation.img_url})${newTMPL}\n\n[#${affiliation.category.trim().replace(/ /g, '')}]\n_ Publicato il ${affiliation.date} _`;
+								TELEGRAM.sendMessage(CHANNEL_ID, newTMPL, { parse_mode : 'markdown' }).then(() => {
+									TELEGRAM.sendMessage(MASTER_ID, `Post ${path} pubblicato!!`, { parse_mode : 'html' });
+								}).catch((error) => {
+									TELEGRAM.sendMessage(MASTER_ID, `ERRORE Post ${path} NON pubblicato!!\n${JSON.stringify(error)}`, { parse_mode : 'html' });
+								});
+								removeFile(path);
+							})
+							.catch(() => {
+								removeFile(path);
+							});
+						}
+						else{
+							TELEGRAM.sendMessage(MASTER_ID, `URL AFFILIAZIONE NON RECUPERATO Post ${path} !!`, { parse_mode : 'html' });
+						}
 					}
 					else{
 						TELEGRAM.sendMessage(MASTER_ID, `Post ${path} contains NO AMAZON URL!!`, { parse_mode : 'html' });
